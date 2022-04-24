a040c326befbd194e66afa68f693ae4e79cd218a
PhuongTran2612 <91450817+PhuongTran2612@users.noreply.github.com>
2022-04-24 12:29:44 +0700

    2000

---
+++ WedBanQuanAo/WedBanQuanAo/Controllers/SanPhamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using WedBanQuanAo.Models;
+namespace WedBanQuanAo.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        //
+        // GET: /SanPham/
+
+        ShopQuanAoDataContext db = new ShopQuanAoDataContext();
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+    }
+}
